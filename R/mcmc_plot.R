229b4b7232a63e61c5400d1a5677de5180ced71a
Trinh Dong <46802143+trinhdhk@users.noreply.github.com>
2022-04-28 16:02:59 +0100

    add multi_point_est support

--- R/mcmc_plot.R
+++ R/mcmc_plot.R
@@ -3,34 +3,60 @@
 #' provides support for pooling several intervals from several fits.
 #' @param fits a list of stan fits, must be wrapped in \code{list()}
 #' @param ... other params passed to \code{bayesplot::mcmc_intervals_data}
+#' @param multi_point_est default to FALSE, whether to set show multiple point est, if TRUE, must specify which point_est you want to plot
 #' @export
 mcmc_intervals_multi <-
   rlang::new_function(
     args = append(
-      list(fits = list()),
+      list(fits = list(), multi_point_est = FALSE),
       formals(bayesplot::mcmc_intervals)[-1]
       ),
     body = quote({
       w <- .15 * length(fits)
-      if (!length(names(fits)))
-        names(fits) <- as.character(as.list(substitute(fits)[-1]))
+
+      if (!length(names(fits))){
+        names(fits) <- seq_along(x)
+      }
+
       plot_call <- sys.call()
       plot_call[[1]] <- quote(bayesplot::mcmc_intervals_data)
       if (!'fits' %in% names(plot_call)) plot_call[[2]] <- NULL
       else plot_call[['fits']] <- NULL
+      if (!'point_est' %in% names(plot_call)) multi_point_est <- FALSE
+      point_est <- if (multi_point_est) unique(plot_call[['point_est']]) else NULL
       est  <- sapply(seq_along(fits),
                      \(i) {
                        this_call <- plot_call
                        this_call[['x']] <- fits[[i]]
-                       eval(this_call) |>
-                         dplyr::mutate(Model = names(fits)[[i]])
+                       if (multi_point_est) {
+                         this_call[['point_est']] <- point_est[[1]]
+                         dt <- eval(this_call) |>
+                           dplyr::mutate(Model = names(fits)[[i]])
+                         for (j in seq_along(point_est)[-1]){
+                           this_call[['point_est']] <- point_est[[j]]
+                           dt[[paste0('m',j)]] <- eval(this_call)$m
+                         }
+                       } else{
+                         dt <- eval(this_call) |>
+                           dplyr::mutate(Model = names(fits)[[i]])
+                       }
+                       dt
                      }, simplify=FALSE, USE.NAMES = TRUE)
       est_dat <- dplyr::bind_rows(est) |> dplyr::mutate(Model = factor(Model, levels = names(fits)))
-      ggplot(est_dat, aes(y=parameter, color=Model)) +
+      plt <- ggplot(est_dat, aes(y=parameter, color=Model)) +
         geom_linerange(aes(xmin=ll, xmax=hh), position=position_dodge(width=w)) +
         geom_linerange(aes(xmin=l, xmax=h), size=1, position=position_dodge(width=w)) +
-        geom_point(aes(x=m), size=1.2, position=position_dodge(width=w)) +
-        xlab('Estimation') + ylab('Parameter')
+        geom_point(aes(x=m), size=point_size, shape=1, position=position_dodge(width=w)) +
+        xlab('Estimate') + ylab('Parameter')
+
+      if (multi_point_est){
+        for (j in seq_along(point_est)[-1]){
+          plt <- plt +
+            geom_point(aes(x=.data[[paste0(m,j)]]), size=point_size, shape=j, position=position_dodge(width=w))
+        }
+      }
+
+      plt
     })
   )
 
